c6f8e0737fe905b6bf05ee92f8af723066b9b68e
Aki Nakahara <akinakahara1515@gmail.com>
2025-12-27 20:44:06 +0900

    add eslint config

---
+++ eslint.config.js
@@ -0,0 +1,26 @@
+import js from '@eslint/js';
+import prettier from 'eslint-plugin-prettier';
+import prettierConfig from 'eslint-config-prettier';
+
+export default [
+  {
+    files: ['**/*.js'],
+    ignores: ['node_modules/**', 'dist/**'],
+
+    languageOptions: {
+      ecmaVersion: 'latest',
+      sourceType: 'module',
+    },
+
+    plugins: {
+      prettier,
+    },
+
+    rules: {
+      ...js.configs.recommended.rules,
+      ...prettierConfig.rules,
+
+      'prettier/prettier': 'error',
+    },
+  },
+];
