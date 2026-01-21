3a4277d38bc7858a3ba40329b48d1460cfa74d5d
Tousif Habib <thabib@mun.ca>
2026-01-21 12:41:41 +0900

    new: obfuscate JS

--- webpack.config.cjs
+++ webpack.config.cjs
@@ -2,49 +2,71 @@ const path = require('path');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
 const MiniCssExtractPlugin = require('mini-css-extract-plugin');
 const TerserPlugin = require('terser-webpack-plugin');
+const WebpackObfuscator = require('webpack-obfuscator');
 
 module.exports = {
-  entry: {
-    main: ['./js/index.js', './css/styles.css', './css/gantt.css'],
-  },
-  output: {
-    path: path.resolve(__dirname, 'dist'),
-    filename: 'bundle.[contenthash].js',
-    clean: true,
-  },
-  module: {
-    rules: [
-      {
-        test: /\.css$/,
-        use: [MiniCssExtractPlugin.loader, 'css-loader'],
-      },
-      {
-        test: /\.(woff|woff2|eot|ttf|otf)$/i,
-        type: 'asset/resource',
-        generator: {
-          filename: 'fonts/[name][ext][query]',
-        },
-      },
+    entry: {
+        main: ['./js/index.js', './css/styles.css', './css/gantt.css'],
+    },
+
+    output: {
+        path: path.resolve(__dirname, 'dist'),
+        filename: 'bundle.[contenthash].js',
+        clean: true,
+    },
+
+    module: {
+        rules: [
+            {
+                test: /\.css$/,
+                use: [MiniCssExtractPlugin.loader, 'css-loader'],
+            },
+            {
+                test: /\.(woff|woff2|eot|ttf|otf)$/i,
+                type: 'asset/resource',
+                generator: {
+                    filename: 'fonts/[name][ext][query]',
+                },
+            },
+        ],
+    },
+
+    optimization: {
+        minimize: true,
+        minimizer: [
+            new TerserPlugin({
+                parallel: false,
+            }),
+        ],
+    },
+
+    plugins: [
+        new WebpackObfuscator(
+            {
+                rotateStringArray: true,
+                stringArray: true,
+                stringArrayThreshold: 0.75,
+                deadCodeInjection: true,
+                deadCodeInjectionThreshold: 0.4,
+                controlFlowFlattening: true,
+                controlFlowFlatteningThreshold: 0.75,
+                splitStrings: true,
+                splitStringsChunkLength: 5,
+                simplify: true,
+                disableConsoleOutput: true,
+            }
+        ),
+
+        new HtmlWebpackPlugin({
+            template: './index.html',
+            minify: {
+                removeComments: true,
+                collapseWhitespace: true,
+            },
+        }),
+
+        new MiniCssExtractPlugin({
+            filename: 'styles.[contenthash].css',
+        }),
     ],
-  },
-  optimization: {
-    minimize: true,
-    minimizer: [
-      new TerserPlugin({
-        parallel: false,
-      }),
-    ],
-  },
-  plugins: [
-    new HtmlWebpackPlugin({
-      template: './index.html',
-      minify: {
-        removeComments: true,
-        collapseWhitespace: true,
-      },
-    }),
-    new MiniCssExtractPlugin({
-      filename: 'styles.[contenthash].css',
-    }),
-  ],
 };
